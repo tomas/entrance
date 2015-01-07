9b5b23bd41f8bb980500449b00263ed7127fa5e3
Tomás Pollak <tomaspollak@gmail.com>
2015-01-07 02:19:57 -0300

    Update entrance.rb

--- examples/rails-app/config/initializers/entrance.rb
+++ examples/rails-app/config/initializers/entrance.rb
@@ -1,9 +1,5 @@
-puts 'Initializing Entrance...'
-
 Entrance.configure do |config|
   config.remember_for              = 1.month
-  config.cipher                    = Entrance::Ciphers::SHA1
-  config.secret                    = 'somethingveryveryveryveryverysecret'
   config.access_denied_redirect_to = '/login'
   config.cookie_secure             = Rails.env.production?
 end
