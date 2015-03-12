7fc89b58389aea9d3cbf5a00e2b449183bd54ffd
Tomás Pollak <tomaspollak@gmail.com>
2015-03-12 12:57:04 -0700

    Added .validate_entrance! method, so we can include Entrance::Model at the top.

--- lib/entrance/model.rb
+++ lib/entrance/model.rb
@@ -7,56 +7,58 @@ module Entrance
       # then login_by_session wont work, nor the ClassMethods below.
       # won't work so we cannot continue.
       unless base.respond_to?(:where)
-        raise "#{base.name} does not have a .where() class method. Cannot continue."
-      end
-
-      fields = if base.respond_to?(:columns)   # ActiveRecord::Base
-        base.columns.collect(&:name)
-      elsif base.respond_to?(:keys)            # MongoMapper::Document
-        base.keys.keys
-      else                                     # just get setters in the class
-        base.instance_methods(false).select { |m| m[/\=$/] }.map { |s| s.to_s.sub('=', '') }
-      end.map { |el| el.to_sym }
-
-      %w(username_attr password_attr).each do |key|
-        field = Entrance.config.send(key)
-        unless fields.include?(field.to_sym)
-          raise "Couldn't find '#{field}' in #{base.name} model."
-        end
-      end
-
-      %w(remember reset).each do |what|
-        if field = Entrance.config.send("#{what}_token_attr")
-          until_field = Entrance.config.send("#{what}_until_attr")
-
-          unless fields.include?(field.to_sym)
-            raise "No #{Entrance.config.send("#{what}_token_attr")} field found. \
-                   Set the config.#{what}_token_attr option to nil to disable the #{what} option."
-          end
-
-          if until_field
-            unless fields.include?(until_field.to_sym)
-              raise "Couldn't find a #{Entrance.config.send("#{what}_until_attr")} field. Cannot continue."
-            end
-          else
-            puts "Disabling expiration timestamp for the #{what} option. This is a VERY bad idea."
-          end
-
-          Entrance.config.can?(what, true)
-          base.send(:include, what.to_sym == :remember ? RememberMethods : ResetMethods)
-        end
-      end
-
-      if base.respond_to?(:validates)
-        base.validates :password, :presence => true, :length => 6..32, :if => :password_required?
-        base.validates :password, :confirmation => true, :if => :password_required?
-        base.validates :password_confirmation, :presence => true, :if => :password_required?
+        raise "#{base.name} does not have a .where() finder class method. Cannot continue."
       end
 
       base.extend(ClassMethods)
     end
 
     module ClassMethods
+
+      def validate_entrance!
+        fields = if self.respond_to?(:columns)   # ActiveRecord::Base
+          columns.collect(&:name)
+        elsif self.respond_to?(:keys)            # MongoMapper::Document
+          keys.keys
+        else                                     # just get setters in the class
+          instance_methods(false).select { |m| m[/\=$/] }.map { |s| s.to_s.sub('=', '') }
+        end.map { |el| el.to_sym }
+
+        %w(username_attr password_attr).each do |key|
+          field = Entrance.config.send(key)
+          unless fields.include?(field.to_sym)
+            raise "Couldn't find '#{field}' in #{base.name} model."
+          end
+        end
+
+        %w(remember reset).each do |what|
+          if field = Entrance.config.send("#{what}_token_attr")
+            until_field = Entrance.config.send("#{what}_until_attr")
+
+            unless fields.include?(field.to_sym)
+              raise "No #{Entrance.config.send("#{what}_token_attr")} field found. \
+                     Set the config.#{what}_token_attr option to nil to disable the #{what} option."
+            end
+
+            if until_field
+              unless fields.include?(until_field.to_sym)
+                raise "Couldn't find a #{Entrance.config.send("#{what}_until_attr")} field. Cannot continue."
+              end
+            else
+              puts "Disabling expiration timestamp for the #{what} option. This is a VERY bad idea."
+            end
+
+            Entrance.config.can?(what, true)
+            self.send(:include, what.to_sym == :remember ? RememberMethods : ResetMethods)
+          end
+        end
+
+        if self.respond_to?(:validates)
+          validates :password, :presence => true, :length => 6..32, :if => :password_required?
+          validates :password, :confirmation => true, :if => :password_required?
+          validates :password_confirmation, :presence => true, :if => :password_required?
+        end
+      end
 
       def authenticate(username, password)
         return if username.blank? or password.blank?
