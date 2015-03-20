6096357da76aac80c660a65c3c669769356badac
Tomás Pollak <tomaspollak@gmail.com>
2015-03-20 10:14:30 -0700

    Handle namescoped classes.

--- lib/entrance.rb
+++ lib/entrance.rb
@@ -12,7 +12,7 @@ module Entrance
   end
 
   def self.model
-    @model ||= Kernel.const_get(config.model)
+    @model ||= get_class(config.model)
   end
 
   def self.fields
@@ -27,6 +27,12 @@ module Entrance
   def self.generate_token(length = 40)
     str = Digest::SHA1.hexdigest([Time.now, rand].join)
     str[0..(length-1)]
+  end
+
+  private
+
+  def self.get_class(str)
+    str.split('::').inject(Object) { |mod, name| mod.const_get(name) }
   end
 
 end
